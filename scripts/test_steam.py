c4d2ab28cc2fa0546e6fc41a631851cf530224a8
HubertKozubek <hkozubek00@gmail.com>
2026-01-11 17:36:39 +0100

    Implemenyts review fetching + simple test

--- scripts/test_steam.py
+++ scripts/test_steam.py
@@ -1,21 +1,30 @@
-from voc.ingestion.steam import SteamReviewProvider
+import asyncio
+import sys
+from pathlib import Path
+import logging
+
+project_root = Path(__file__).parent.parent
+sys.path.append(str(project_root / "src"))
+
+from voc.ingestion.providers.steam import SteamReviewProvider
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("test_steam")
 
-def test_fetch():
-    APP_ID = "1030300" # Hollow Knight: Silksong
+async def test_fetch():
+    APP_ID = "2393760" # Random game with small number of reviews
     
     provider = SteamReviewProvider(app_id=APP_ID)
     
-    since_24h = datetime.now(timezone.utc) - timedelta(hours=24)
-    logger.info(f"Testing incremental fetch since {since_24h}")
+    logger.info(f"Testing fetch of all reviews")
     
-    reviews = provider.fetch_reviews(since=since_24h)
+    reviews = [r async for r in provider.fetch_reviews()]
     
     logger.info(f"Count: {len(reviews)}")
     if reviews:
-        logger.info(f"Sample content: {reviews[0].content[:50]}...")
+        logger.info(f"Sample content: {reviews[0]['review'][:50]}...")
+
+    assert len(reviews) > 100 # At the time of writing, this game has 475 reviews
 
 if __name__ == "__main__":
-    test_fetch()
+    asyncio.run(test_fetch())
--- src/voc/ingestion/providers/steam.py
+++ src/voc/ingestion/providers/steam.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import urllib.parse
 from typing import List, Optional, Any, Dict
 from datetime import datetime, timezone
 import time
@@ -8,10 +9,36 @@ from .provider import BaseReviewProvider, Review
 logger = logging.getLogger(__name__)
 
 class SteamReviewProvider(BaseReviewProvider):
-    BASE_URL = "https://store.steampowered.com/appreviews/"
+    BASE_URL = "https://store.steampowered.com/appreviews/{appid}"
+    DEFAULT_PARAMETERS = {
+        "json": 1,
+        "language": "english",
+        "num_per_page": 100,
+        "filter": "recent",
+        "cursor": "*"
+    }
 
-    def __init__(self, app_id: str):
+    def __init__(self, app_id: str, params: dict | None = None):
         self.app_id = app_id
+        self.params = params or self.DEFAULT_PARAMETERS
 
-    def fetch_reviews(self, since: datetime | None = None) -> List[Review]:
-        pass
+    async def fetch_reviews(self, since: datetime | None = None) -> List[Review]:
+        url = self.BASE_URL.format(appid=self.app_id)
+        params = self.params.copy()
+        num_of_reviews_retrived = 100
+
+        while num_of_reviews_retrived != 0:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+
+            data = response.json()
+            reviews_data = data.get("reviews", [])
+            num_of_reviews_retrived = data["query_summary"]["num_reviews"]
+
+            for r in reviews_data:
+                yield r
+
+            cursor = urllib.parse.quote(data.get("cursor", ""))
+            params["cursor"] = cursor
+            
+            
